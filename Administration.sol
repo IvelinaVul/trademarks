7d11fe7d8fc00c427856a1bd2a857d314d07fdf0
MagiTV <43250857+MagiTV@users.noreply.github.com>
2021-01-29 20:53:32 +0200

    delete trademarks and auctions arrays

--- Administration.sol
+++ Administration.sol
@@ -31,18 +31,23 @@ contract Administration {
     
     address payable private owner;
     
-    string[] private trademarks;
-    string[] private activeAuctionNames;  //trademarksWithActiveAuction
-    
-    mapping (string => Trademark) private trademarksNames;
+    mapping (string => Trademark) private trademarks;
     mapping (string => Auction) activeAuctions;
     
     constructor() {
         owner = msg.sender;
     }
+        
+    function getPriceByYear() external pure returns (uint256) {
+        return priceByYear;
+    }
+    
+    function getPriceForUpdate() external pure returns (uint256) {
+        return priceForUpdate;
+    }
     
     modifier isOwnerTrademark(string memory trademarkName) {
-        require(msg.sender == trademarksNames[trademarkName].owner, "Caller is not owner!");
+        require(msg.sender == trademarks[trademarkName].owner, "Caller is not owner!");
         _;
     }
     
@@ -82,164 +87,81 @@ contract Administration {
         newTrademark.owner = msg.sender;
         newTrademark.officialSite = _officialSite;
         
-        trademarksNames[_name] = newTrademark;
-        trademarks.push(_name);
+        trademarks[_name] = newTrademark;
         
         owner.transfer(msg.value);
     }
     
-    function getPriceByYear() external pure returns (uint256) {
-        return priceByYear;
-    }
-    
-    function getPriceForUpdate() external pure returns (uint256) {
-        return priceForUpdate;
-    }
-    
     modifier futureStartDateTrademark(string memory name) {
-        require(block.timestamp <= trademarksNames[name].startDate, "Not enough money!");
+        require(block.timestamp <= trademarks[name].startDate, "Not enough money!");
         _; 
     }
     
     function checkAvailableTrademarkName(string memory name) public view returns (bool) {
-        if(trademarksNames[name].exists == false) {
+        if(trademarks[name].exists == false) {
             return true;
         }
-        return block.timestamp > trademarksNames[name].startDate + trademarksNames[name].term * 365 * 24 * 60 * 60;
+        return block.timestamp > trademarks[name].startDate + trademarks[name].term * 365 * 24 * 60 * 60;
     }
     
     function extendTerm(string memory trademarkName, uint8 newTerm) external payable isOwnerTrademark(trademarkName) enoughMoney(newTerm * priceByYear) {
-        trademarksNames[trademarkName].term += newTerm;
+        trademarks[trademarkName].term += newTerm;
         owner.transfer(msg.value);
     }
     
     function updateData(string memory trademarkName, uint256 newStartDate) external payable
                             isOwnerTrademark(trademarkName) enoughMoney(priceForUpdate) futureDate(newStartDate) futureStartDateTrademark(trademarkName) {
-        trademarksNames[trademarkName].startDate = newStartDate;
+        trademarks[trademarkName].startDate = newStartDate;
         owner.transfer(msg.value);
     }
     
     function updateDescription(string memory trademarkName, string memory newDescription) external payable
                                 isOwnerTrademark(trademarkName) enoughMoney(priceForUpdate) {
-        trademarksNames[trademarkName].description = newDescription;
+        trademarks[trademarkName].description = newDescription;
         owner.transfer(msg.value);
     }
     
     function updateCategory(string memory trademarkName, string memory newCategory) external payable
                             isOwnerTrademark(trademarkName) enoughMoney(priceForUpdate){
-        trademarksNames[trademarkName].category = newCategory;
+        trademarks[trademarkName].category = newCategory;
         owner.transfer(msg.value);
     }
     
     function updateOfficialSite(string memory trademarkName, string memory newSite) external payable
                                 isOwnerTrademark(trademarkName) enoughMoney(priceForUpdate){
-        trademarksNames[trademarkName].officialSite = newSite;
+        trademarks[trademarkName].officialSite = newSite;
         owner.transfer(msg.value);
     }
     
     function daysUntilAvailableTrademarkName(string memory name) external view returns (uint256) {
-        if(trademarksNames[name].exists == false) {
+        if(trademarks[name].exists == false) {
             return 0;
         }
-        return trademarksNames[name].startDate / 60 / 60 / 24 + trademarksNames[name].term * 365;
+        return trademarks[name].startDate / 60 / 60 / 24 + trademarks[name].term * 365;
     }
         
-    modifier isActiveAuction(string memory trademarkName) {
-    	require(address(activeAuctions[trademarkName])!=address(0) && activeAuctions[trademarkName].isActive(), "There is no active auction with that name!");
-        _;
-    }
-    
-    modifier isNotActiveAuction(string memory trademarkName) {
-    	require(address(activeAuctions[trademarkName])==address(0) || activeAuctions[trademarkName].isActive()  == false, "There is no active auction with that name!");
-        _;
-    }
-  
-    function createAuction(string memory trademarkName, uint128 initialPrice, uint128 minBidAmount) external isOwnerTrademark(trademarkName) isNotActiveAuction(trademarkName) {
-     	Trademark memory trademark = trademarksNames[trademarkName];
-     	Auction auction = new Auction(trademark.name, trademark.owner, minBidAmount, initialPrice);
-     	activeAuctions[trademarkName] = auction;
-        activeAuctionNames.push(trademarkName);
-        auction.start();
-    }
-    
-    function compareStrings(string memory a, string memory b) private pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-    }
-    
-    function deleteAuction(string memory trademarkName) private {
-    	delete activeAuctions[trademarkName];
-        string[] memory copyActiveAuctionNames = new string[](activeAuctionNames.length - 1);
-        uint j = 0;
-        for (uint i = 0; i < activeAuctionNames.length; i++) {
-         	if (!compareStrings(activeAuctionNames[i], trademarkName)) {
-            	copyActiveAuctionNames[j] = activeAuctionNames[i];
-            	j++;
-            }
-        }
-        
-        delete activeAuctionNames;
-        for (uint i = 0; i < copyActiveAuctionNames.length; i++) {
-         	activeAuctionNames.push(copyActiveAuctionNames[i]);
-        }
-        delete copyActiveAuctionNames;
-    }
-    
-    function closeAuction(string memory trademarkName) external isActiveAuction(trademarkName) isOwnerTrademark(trademarkName) {
-        Auction auction = activeAuctions[trademarkName];
-        auction.close();
-        uint256 highestPrice=auction.getHighestPrice();
-        uint256 fee = uint256(highestPrice * 5 / 100);
-
-        address highestBidder=auction.getHighestBidder();
-        Trademark memory trademark=trademarksNames[trademarkName];
-        
-        if (fee > 0) {
-  			owner.transfer(fee);
-        }
-       
-    	payTo(payable(trademark.owner), highestPrice - fee);
-    	trademark.owner=highestBidder;
-        deleteAuction(trademarkName);
-    }
-    
-    function payTo(address payable toAddress, uint256 amount) public payable {
-    	 toAddress.transfer(amount);	 
-    }
-    
-    function participateInAuction(string memory trademarkName) external payable isActiveAuction(trademarkName) { 
-        Auction auction = activeAuctions[trademarkName];
-        auction.bid{value:msg.value}(msg.sender);
-    } 
-    
     function isTrademarkInDate(string memory trademarkName) public view isTrademarkRegistered(trademarkName) returns(bool){
-        if(block.timestamp < trademarksNames[trademarkName].startDate) {
+        if(block.timestamp < trademarks[trademarkName].startDate) {
             return false;
         }
         return true;
     }
+        
+    modifier isOwnerAdministration() {
+        require(msg.sender == owner, "Caller is not owner!");
+        _;
+    }
     
+    //TODO: 50% refund
     function giveUpOnPatent(string memory trademarkName) external payable
-                            isOwnerTrademark(trademarkName) futureDate(trademarksNames[trademarkName].startDate) enoughMoney(priceForGiveUp) {
-        delete trademarksNames[trademarkName];
-        string[] memory copyTrademarkNames = new string[](trademarks.length - 1);
-        uint j = 0;
-        for (uint i = 0; i < trademarks.length; i++) {
-         	if (!compareStrings(trademarks[i], trademarkName)) {
-                copyTrademarkNames[j] = trademarks[i];
-            	j++;
-            }
-        }
-        delete trademarks;
-        for (uint i = 0; i < copyTrademarkNames.length; i++) {
-         	trademarks.push(copyTrademarkNames[i]);
-        }
-        delete copyTrademarkNames;
+                            isOwnerTrademark(trademarkName) futureDate(trademarks[trademarkName].startDate) enoughMoney(priceForGiveUp) {
         owner.transfer(msg.value);
+        delete trademarks[trademarkName];
     }
 
     function isSiteAuthorized(string memory site,string memory trademarkName) public view isTrademarkRegistered(trademarkName) returns (bool){
-       for(uint i = 0; i < trademarksNames[trademarkName].authorizedSites.length; ++i) {
-           if(compareStrings(trademarksNames[trademarkName].authorizedSites[i],site)) {
+       for(uint i = 0; i < trademarks[trademarkName].authorizedSites.length; ++i) {
+           if(compareStrings(trademarks[trademarkName].authorizedSites[i],site)) {
                return true;
            }
        }
@@ -253,7 +175,57 @@ contract Administration {
 
     function addAuthroizedSite(string memory authorizedSite,string memory trademarkName) external payable
                                 isOwnerTrademark(trademarkName) enoughMoney(priceForUpdate) isSiteAlreadyAuthorized(authorizedSite,trademarkName){
-        trademarksNames[trademarkName].authorizedSites.push(authorizedSite);
+        trademarks[trademarkName].authorizedSites.push(authorizedSite);
         owner.transfer(msg.value);
     }
+    
+    //*** AUCTIONS ***//
+        
+    modifier isActiveAuction(string memory trademarkName) {
+    	require(address(activeAuctions[trademarkName])!=address(0) && activeAuctions[trademarkName].isActive(), "There is no active auction with that name!");
+        _;
+    }
+    
+    modifier isNotActiveAuction(string memory trademarkName) {
+    	require(address(activeAuctions[trademarkName])==address(0) || activeAuctions[trademarkName].isActive()  == false, "There is no active auction with that name!");
+        _;
+    }
+  
+    function createAuction(string memory trademarkName, uint128 initialPrice, uint128 minBidAmount) external isOwnerTrademark(trademarkName) isNotActiveAuction(trademarkName) {
+     	Trademark memory trademark = trademarks[trademarkName];
+     	Auction auction = new Auction(trademark.name, trademark.owner, minBidAmount, initialPrice);
+     	activeAuctions[trademarkName] = auction;
+        auction.start();
+    }
+    
+    function compareStrings(string memory a, string memory b) private pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+    
+    function deleteAuction(string memory trademarkName) private {
+    	delete activeAuctions[trademarkName];
+    }
+    
+    function closeAuction(string memory trademarkName) external isActiveAuction(trademarkName) isOwnerTrademark(trademarkName) {
+        Auction auction = activeAuctions[trademarkName];
+        auction.close();
+        uint256 highestPrice=auction.getHighestPrice();
+        uint256 fee = uint256(highestPrice * 5 / 100);
+
+        address highestBidder=auction.getHighestBidder();
+        Trademark memory trademark=trademarks[trademarkName];
+        
+        if (fee > 0) {
+  			owner.transfer(fee);
+        }
+       
+        payable(trademark.owner).transfer(highestPrice - fee);
+    	trademark.owner=highestBidder;
+        deleteAuction(trademarkName);
+    }
+    
+    function participateInAuction(string memory trademarkName) external payable isActiveAuction(trademarkName) { 
+        Auction auction = activeAuctions[trademarkName];
+        auction.bid{value:msg.value}(msg.sender);
+    } 
 }
